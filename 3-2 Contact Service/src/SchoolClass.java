11265ba568a4d0fe4461cfa78b55da28714bfead
Kubia-Beta <113896543+Kubia-Beta@users.noreply.github.com>
2024-07-21 22:00:02 -0700

    Refactor SchoolClass
    made name changes, added tooltips, clarified variable names, separated code into blocks with comments

--- 3-2 Contact Service/src/SchoolClass.java
+++ 3-2 Contact Service/src/SchoolClass.java
@@ -1,6 +1,8 @@
-/**
- * 
- */
+////////////////////////////
+// By:                   //
+// Connor Sculthorpe    //
+// 21 June 2024        //
+////////////////////////
 
 /**
  * @author connorscultho_snhu
@@ -11,68 +13,107 @@ public class SchoolClass {
 	public String firstName;
 	public String lastName;
 	public String phoneNumber; // String for now, this may change to int later? Check wk.6
-	public String address;
+	public String address; // This should be verified to exist in the customer country if possible (USPS format?)
 
 	// Get and set ID
+	/**
+	 * @param N/A
+	 * @return string ID
+	 */
 	public String getID() {
 		return ID;
 	}
 
-	public void setID(String iD) {
-		if(iD == null || iD.length() > 10) { // Validate ID exists and length is <10
+	/**
+	 * @param string New ID
+	 * @return N/A
+	 */
+	public void setID(String NewID) {
+		if(NewID == null || NewID.length() > 10) { // Validate ID exists and length is <=10
 			throw new IllegalArgumentException("Invalid ID");
 		}
-		ID = iD; // Must be valid to reach
+		ID = NewID; // Must be valid to reach
 	}
 	
 	// Get and set firstName
+	/**
+	 * @param N/A
+	 * @return string First Name
+	 */
 	public String getfirstName() {
 		return firstName;
 	}
 
-	public void setfirstName(String name) {
-		if(name == null || name.length() > 10) {
+	public void setfirstName(String newFirstName) {
+		if(newFirstName == null || newFirstName.length() > 10) {
 			throw new IllegalArgumentException("Invalid First Name");
 		}
-		firstName = name;
+		firstName = newFirstName;
 	}
 	
 	// Get and set lastName
+	/**
+	 * @param N/A
+	 * @return string Last Name
+	 */
 	public String getLastName() {
 		return lastName;
 	}
-
-	public void setLastName(String name) {
-		if(name == null || name.length() > 10) {
+	
+	/**
+	 * @param string New Last Name
+	 * @return N/A
+	 */
+	public void setLastName(String newLastName) {
+		if(newLastName == null || newLastName.length() > 10) {
 			throw new IllegalArgumentException("Invalid Last Name");
 		}
-		lastName = name;
+		lastName = newLastName;
 	}
 	
 	// Get and set phone
+	/**
+	 * @param N/A
+	 * @return string phone
+	 */
 	public String getPhone() {
 		return phoneNumber;
 	}
 
-	public void setPhone(String phone) {
-		if(phone == null || phone.length() == 9) {
+	/**
+	 * @param string New Phone
+	 * @return N/A
+	 */
+	public void setPhone(String newPhone) {
+		if(newPhone == null || newPhone.length() == 9) {
 			throw new IllegalArgumentException("Invalid Phone Number");
 		}
-		phoneNumber = phone;
+		phoneNumber = newPhone;
 	}
 	
-	// Get and set phone
+	// Get and set address
+	/**
+	 * @param N/A
+	 * @return string address
+	 */
 	public String getAddress() {
-		return phoneNumber;
+		return address;
 	}
 
-	public void setAddress(String curAddress) {
-		if(curAddress == null || curAddress.length() == 9) {
+	/**
+	 * @param string New Address
+	 * @return N/A
+	 */
+	public void setAddress(String newAddress) {
+		if(newAddress == null || newAddress.length() == 30) { // Strangely does NOT have a min length in requirements
 			throw new IllegalArgumentException("Invalid Address");
 		}
-		address = curAddress;
+		address = newAddress;
 	}
 	
+	//====================================
+	// Main
+	//====================================
 	/**
 	 * @param args
 	 */
