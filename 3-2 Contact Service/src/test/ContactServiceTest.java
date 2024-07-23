59f77b6b8e6ca2b94e7eca5beb7eb7725b0b3d11
Kubia-Beta <113896543+Kubia-Beta@users.noreply.github.com>
2024-07-22 20:15:35 -0700

    Create ContactServiceTest.java

---
+++ 3-2 Contact Service/src/test/ContactServiceTest.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author connorscultho_snhu
+ *
+ */
+class ContactServiceTest {
+	@Test
+	void testCreate() {
+		
+	}
+	
+	@Test
+	void testCreateDuplicate() {
+		
+	}
+	
+	@Test
+	void testCreateTooLongAllFields() {
+		
+	}
+	
+	@Test
+	void testDeleteExists() {
+		
+	}
+	
+	@Test
+	void testDeleteNoExist() {
+		
+	}
+	
+	@Test
+	void testUpdateInvalidID() {
+		
+	}
+	
+	@Test
+	void testUpdateInvalidFirst() {
+		
+	}
+	
+	@Test
+	void testUpdateInvalidLast() {
+		
+	}
+	
+	@Test
+	void testUpdateInvalidPhone() {
+		
+	}
+	
+	@Test
+	void testUpdateInvalidAddress() {
+		
+	}
+	
+	@Test
+	void testUpdateInvalidOutOfRange() {
+		
+	}
+	
+	@Test
+	void testUpdateValidFirst() {
+		
+	}
+	
+	@Test
+	void testUpdateValidLast() {
+		
+	}
+	
+	@Test
+	void testUpdateValidPhone() {
+		
+	}
+	
+	@Test
+	void testUpdateValidAddress() {
+		
+	}
+}
